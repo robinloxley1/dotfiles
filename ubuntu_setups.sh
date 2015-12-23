95bd92c9324f63fcb30507fc2b601cb64777d194
robinloxley1 <robinloxley1@hotmail.com>
2015-12-24 00:06:16 +0800

    add java

--- ubuntu_setups.sh
+++ ubuntu_setups.sh
@@ -9,11 +9,17 @@ setup_docker() {
 setup_commons () {
   sudo apt-get update
   sudo apt-get install -y vim git tmux curl wget unzip build-essential libssl-dev libffi-dev python-dev
+  sudo apt-get install python-software-properties
+  sudo apt-get install oracle-java7-installer 
 }
 
 setup_repo () {
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   sudo bash -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
+  # for Oracle JDK
+  sudo add-apt-repository ppa:webupd8team/java
+
+  sudo apt-get update
 }
 
 setup_python() {
